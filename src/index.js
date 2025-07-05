cb4eb4365e9f04d5dc702a847c504af4234dba1b
King-Code-938 <danielidowu938@gmail.com>
2025-07-05 01:08:24 +0100

    serviceWorker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { register } from './serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -16,4 +16,6 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-register();
+
+serviceWorkerRegistration.register();
+
