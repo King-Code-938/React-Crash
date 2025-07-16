9707848c76e3708757f0415de5e1929042b04b16
Daniel Oluwatobiloba Idowu <danielidowu938@gmail.com>
2025-07-17 00:40:01 +0100

    fix: streamline response handling in login and register functions

--- src/services/authApi.js
+++ src/services/authApi.js
@@ -7,11 +7,11 @@ export const login = (url, email, password) =>
     method: 'POST',
     headers: getHeaders(),
     body: JSON.stringify({ email: email, password: password }),
-  });
+  }).then(res => res.json());
 
 export const register = (url, username, email, inviteCode, password) =>
   fetch(`${url}/register`, {
     method: 'POST',
     headers: getHeaders(),
     body: JSON.stringify({ username: username, email: email, inviteCode: inviteCode, password: password }),
-  });
+  }).then(res => res.json());
